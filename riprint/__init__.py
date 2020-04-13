9d3e38884db70a0d22f9ea4860dcf94ec22c262b
Hampus Hallman <hampus@hallman.eu>
2020-04-13 02:17:53 +0200

    various improvements

--- riprint/__init__.py
+++ riprint/__init__.py
@@ -6,24 +6,27 @@ def _riprint(value, color, indent=0):
   if isinstance(value, (int, float)):
     return colored(value, color or 'yellow')
   elif isinstance(value, str):
-    return colored(f"'{value}'", color or 'green')
+    if indent == 0:
+      return colored(f"{value}", color)
+    else:
+      return colored(f"'{value}'", color or 'green')
   elif isinstance(value, tuple):
     child_str = ', '.join([_riprint(val, color, indent) for val in value])
     return f"({child_str})"
   elif isinstance(value, list):
     children = [_riprint(val, color, indent + 1) for val in value]
-    cand_text = ', '.join(children)
-    if len(cand_text) <= 80 and '\n' not in cand_text:
-      return f"[{cand_text}]"
+    child_str = ', '.join(children)
+    if len(child_str) <= 80 and '\n' not in child_str:
+      return f"[{child_str}]"
     else:
       children = [next_indent_str + child for child in children]
       child_str = ',\n'.join(children)
       return f"[\n{child_str}\n{indent_str}]"
   elif isinstance(value, dict):
-    children = [next_indent_str + _riprint(key, color) + ': ' + _riprint(val, color, indent + 1) for key, val in value.items()]
+    children = [next_indent_str + _riprint(key, color, indent + 1) + ': ' + _riprint(val, color, indent + 1) for key, val in value.items()]
     child_str = ',\n'.join(children)
     return f"{{\n{child_str}\n{indent_str}}}"
-  return value
+  return value.__str__()
 
-def riprint(*values, color=None):
-  print(*(_riprint(value, color) for value in values))
+def riprint(*values, color=None, print=print, **kwargs):
+  print(*(_riprint(value, color) for value in values), **kwargs)
