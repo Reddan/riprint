5a4328463a59ce8efbc9f29629e7c6480ebc5b19
Hampus Hallman <hampus@hallman.eu>
2020-04-13 02:18:10 +0200

    version bumped to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='riprint',
   packages=['riprint'],
-  version='1.0.0',
+  version='1.0.1',
   author='Hampus Hallman',
   author_email='me@hampushallman.com',
   url='https://github.com/Reddan/riprint',
